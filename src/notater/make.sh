6fdf32b79a60ffcb28ca69531bdb87ce3b33ae09
Mikael Mortensen <mikaem@math.uio.no>
2017-09-12 14:36:18 +0200

    Adding figs

--- src/notater/make.sh
+++ src/notater/make.sh
@@ -1,4 +1,5 @@
 doconce format html index --html_style=solarized2_light
 doconce format html index --html_style=bootstrap --html_output=index_bootstrap
 cp index.html ../../docs/
+#cp -r figs/*.* ../../docs/
 
