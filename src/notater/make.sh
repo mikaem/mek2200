96174c8d1a20f3acd7f7218b60f5786c7b2c5b3d
Mikael Mortensen <mikaem@math.uio.no>
2018-09-06 10:11:30 +0200

    Updating oblig 1 to 2018

--- src/notater/make.sh
+++ src/notater/make.sh
@@ -2,4 +2,11 @@ doconce format html index --html_style=solarized2_light
 doconce format html index --html_style=bootstrap --html_output=index_bootstrap
 cp index.html ../../docs/
 #cp -r figs/*.* ../../docs/
+doconce format pdflatex index --latex_code_style=pyg --encoding=utf-8
+doconce replace "\subsection" "\section" index.tex
+pdflatex index
+pdflatex index
+pdflatex index
+
+mv index.pdf notes_on_notation.pdf
 
